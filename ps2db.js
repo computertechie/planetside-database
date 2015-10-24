0d1dc2ee477676b7d04d4155ef4fc2ed5613a4f3
Ean McLaughlin <computertechie2@gmail.com>
2015-10-23 19:49:20 -0600

    Add central module file. Save live events only currently.

--- ps2db.js
+++ ps2db.js
@@ -1,3 +1,25 @@
 /**
  * Created by Pepper on 10/23/2015.
  */
+
+var config = require('./config');
+var mongoose = require('mongoose');
+var Events = require('./lib/Events');
+
+var PS2DB = function () {
+    if (config.db_options.user)
+        mongoose.connect(config.db_uri, config.db_options);
+    else
+        mongoose.connect(config.db_uri);
+    var database = mongoose.connection;
+};
+
+PS2DB.prototype.save = function (event) {
+    var eventDoc = new Events[event.event_type]();
+    eventDoc.save(function (err, dbEvent) {
+        if (err)
+            return console.error(err);
+    });
+};
+
+module.exports = new PS2DB();
